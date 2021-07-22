bc6efaaa427e154825739824f1c8685ce9606e47
陈俊林 <3397920970@qq.com>
2021-07-22 11:10:30 +0800

    push

--- src/SurgePaser.ts
+++ src/SurgePaser.ts
@@ -1,35 +1,40 @@
 const SurgePaser = (surgeConfig: string) => {
   const lineArray = surgeConfig.split("\n")
 
-  const array = []
+  const array = {}
+
+  let name
 
   for(const line of lineArray) {
     //空行 pass
     if (line.length === 0) continue;
     //判断用户
     if (/^\[*\]$/.test(line)) {
-      const name = line.replace(/(\[ | \])/, '')
+      name = line.replace(/(\[ | \])/, '')
+      array[name] = []
     }
+
+    array[name].push(line)
 
   }
 
-  configLine
+  // configLine
 
-  const hang = center.split("\n");
+  // const hang = center.split("\n");
 
-  const hanAa = [];
+  // const hanAa = [];
 
-  for (let han of hang) {
-    if (han.length !== 0) {
-      han = han.replace(" ", "");
-      han.indexOf("=");
-      if (han.indexOf("=") !== -1) {
-        hanAa.push(han.split("="));
-      }
-    }
-  }
+  // for (let han of hang) {
+  //   if (han.length !== 0) {
+  //     han = han.replace(" ", "");
+  //     han.indexOf("=");
+  //     if (han.indexOf("=") !== -1) {
+  //       hanAa.push(han.split("="));
+  //     }
+  //   }
+  // }
 
-  return {};
+  // return {};
 };
 
 export default SurgePaser;
