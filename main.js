c66730245b73529d000c9da1be64d445f7f841b8
Junlin Chen <cutani1997@gmail.com>
2021-08-19 14:25:25 +0800

    push

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
 const yaml = require("js-yaml");
 const fs = require("fs");
 
-// const Nexitally_Clash = fs.readFileSync("./Nexitally_Clash.yml", "utf8");
+const Nexitally_Clash = fs.readFileSync("./proxy.yml", "utf8");
 
-// const object = yaml.load(Nexitally_Clash);
+const object = yaml.load(Nexitally_Clash);
+
+const { proxies } = object["proxy-groups"][0];
+
+console.log(proxies);
 
 const auto = [
-  'https://binance.ankr.com/',
-  'https://bsc-dataseed1.binance.org',
-  'https://bsc-dataseed2.binance.org',
-  'https://bsc-dataseed3.binance.org',
-  'https://bsc-dataseed4.binance.org',
-  'https://bsc-dataseed1.defibit.io',
-  'https://bsc-dataseed2.defibit.io',
-  'https://bsc-dataseed3.defibit.io',
-  'https://bsc-dataseed4.defibit.io',
-  'https://bsc-dataseed1.ninicoin.io',
-  'https://bsc-dataseed2.ninicoin.io',
-  'https://bsc-dataseed3.ninicoin.io',
-  'https://bsc-dataseed4.ninicoin.io',
-]; 
+  "https://binance.ankr.com/",
+  "https://bsc-dataseed1.binance.org",
+  "https://bsc-dataseed2.binance.org",
+  "https://bsc-dataseed3.binance.org",
+  "https://bsc-dataseed4.binance.org",
+  "https://bsc-dataseed1.defibit.io",
+  "https://bsc-dataseed2.defibit.io",
+  "https://bsc-dataseed3.defibit.io",
+  "https://bsc-dataseed4.defibit.io",
+  "https://bsc-dataseed1.ninicoin.io",
+  "https://bsc-dataseed2.ninicoin.io",
+  "https://bsc-dataseed3.ninicoin.io",
+  "https://bsc-dataseed4.ninicoin.io",
+];
 
 // for (const proxy of object.proxies) {
 //   if (proxy.name.indexOf('Premium') !== -1) continue;
@@ -30,28 +34,27 @@ const auto = [
 
 // console.log(Proxy2)
 
-const proxyGroups = []
-const rules = []
+const proxyGroups = [];
+const rules = [];
 
 for (const testUrl of auto) {
-  let name = testUrl.substring(8).substring(-1).split('.').join('-');
+  let name = testUrl.substring(8).substring(-1).split(".").join("-");
 
   proxyGroups.push({
     name,
-    type: 'url-test',
-    proxies: [
-
-    ],
+    type: "url-test",
+    proxies: proxies,
     url: testUrl,
     interval: 300,
-  })
+  });
 
-  rules.push(`DOMAIN,${testUrl},${name}`)
+  rules.push(`DOMAIN,${testUrl},${name}`);
 }
 
+console.log(proxyGroups)
 
 const dump = yaml.dump({
-  'proxy-groups': proxyGroups,
+  "proxy-groups": proxyGroups,
   rules,
 });
 
