f1be5d873f3cce7d2ba0001c5384f3816b910ea2
Junlin Chen <cutani1997@gmail.com>
2021-08-19 10:06:22 +0800

    push

--- main.js
+++ main.js
@@ -1,50 +1,51 @@
 const yaml = require("js-yaml");
 const fs = require("fs");
 
-const Nexitally_Clash = fs.readFileSync("./Nexitally_Clash.yml", "utf8");
+// const Nexitally_Clash = fs.readFileSync("./Nexitally_Clash.yml", "utf8");
 
-const object = yaml.load(Nexitally_Clash);
+// const object = yaml.load(Nexitally_Clash);
 
-const Proxy2 = []; 
+const auto = [
+  'https://binance.ankr.com/',
+  'https://bsc-dataseed1.binance.org',
+  'https://bsc-dataseed2.binance.org',
+  'https://bsc-dataseed3.binance.org',
+  'https://bsc-dataseed4.binance.org',
+  'https://bsc-dataseed1.defibit.io',
+  'https://bsc-dataseed2.defibit.io',
+  'https://bsc-dataseed3.defibit.io',
+  'https://bsc-dataseed4.defibit.io',
+  'https://bsc-dataseed1.ninicoin.io',
+  'https://bsc-dataseed2.ninicoin.io',
+  'https://bsc-dataseed3.ninicoin.io',
+  'https://bsc-dataseed4.ninicoin.io',
+]; 
 
-for (const proxy of object.proxies) {
-  if (proxy.name.indexOf('Premium') !== -1) continue;
-  if (proxy.name.indexOf('Hong Kong') !== -1) {
-    Proxy2.push(proxy.name.trim())
-  }
+// for (const proxy of object.proxies) {
+//   if (proxy.name.indexOf('Premium') !== -1) continue;
+//   if (proxy.name.indexOf('Hong Kong') !== -1) {
+//     Proxy2.push(proxy.name.trim())
+//   }
+// }
+
+// console.log(Proxy2)
+
+const proxyGroups = []
+
+for (const testUrl of auto) {
+  proxyGroups.push({
+    name: testUrl,
+    type: 'url-test',
+    proxies: [
+
+    ],
+    url: testUrl,
+    interval: 300,
+  })
 }
 
-console.log(Proxy2)
-
 const dump = yaml.dump({
-  port: 7890, // https端口号
-  'socks-port': 7891, // socks5端口号
-  'allow-lan': false, // 开启远程访问
-  'log-level': 'info', // 日记
-  profile: {
-    'store-selected': false, // store
-    tracing: true // 监控
-  },
-  //dns
-  dns: {
-    enable: false,
-  },
-  // tun 模式
-  tun: {
-    enable: true,
-    stack: 'system'
-  },
-  //代理
-  proxies: object.proxies,
-  // 代理组
-  'proxy-groups': {
-    Proxy: Proxy2
-  },
-  //规则
-  rules: [
-    'GEOIP,CN,DIRECT',
-    'MATCH,Proxy'
-  ]
+  'proxy-groups': proxyGroups
 });
 
-fs.writeFileSync("./NodeList_Clash.yml", dump);
+fs.writeFileSync("./so.yml", dump);
